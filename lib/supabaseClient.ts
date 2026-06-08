649ed1200aa694f9533fff6aca8eeef0a3df3a23
mntdr66 <mntdr6600@gmail.com>
2026-06-08 20:15:22 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://yllgsdjibuelocvgudds.supabase.co";
-const SUPABASE_KEY = "sb_publishable_fFSdMOUOsMIUtROeSe5AZw__nx6HtTn";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
